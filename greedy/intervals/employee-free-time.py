6d8c0f9ef05d6aa109f1198d2da946471ad2487e
dyf102 <dyf102@gmail.com>
2021-01-29 17:42:40 -0500

    Fix bug of employeefreeTime question

--- greedy/intervals/employee-free-time.py
+++ greedy/intervals/employee-free-time.py
@@ -1,26 +1,24 @@
 from typing import List
 
-def employeeFreeTime(schedule : List[List[int]]):
-    deltas = []
+
+def employeeFreeTime(schedule):
     result = []
-    for individual in schedule:
-        for shift in individual:
-            deltas.append((shift[0], 1))
-            deltas.append((shift[1], -1))
     
-    deltas.sort()
-
-    current = 0
-    start = -1
-    for time, v in deltas:
-        current += v
-        if current == 0:
-            start = time
-        elif start != -1:
-            result.append([start, time])
-            start = -1
+    delta = []
+    for person in schedule:
+        for event in person:
+            delta.append((event[0], 0))
+            delta.append((event[1], 1))
+    delta.sort()
+    status = 0
+    prev = -1
+    for t, v in delta:
+        if prev != -1 and status == 0:
+            result.append([prev, t])
+        status += 1 if v == 0 else -1 
+        prev = t
     return result
-
 if __name__ == "__main__":
-    print(employeeFreeTime([[[1,3],[6,7]],[[2,4]],[[2,5],[9,12]]]))
-    print(employeeFreeTime([[[1,2],[5,6]],[[1,3]],[[4,10]]]))
+    # print(employeeFreeTime([[[1,3],[6,7]],[[2,4]],[[2,5],[9,12]]]))
+    # print(employeeFreeTime([[[1,2],[5,6]],[[1,3]],[[4,10]]]))
+    print(employeeFreeTime([[[7,24],[29,33],[45,57],[66,69],[94,99]],[[6,24],[43,49],[56,59],[61,75],[80,81]],[[5,16],[18,26],[33,36],[39,57],[65,74]],[[9,16],[27,35],[40,55],[68,71],[78,81]],[[0,25],[29,31],[40,47],[57,87],[91,94]]]))
