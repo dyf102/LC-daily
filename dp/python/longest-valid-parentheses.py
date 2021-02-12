04bfe9d6dcca3eb5b4498b7c1d0dae89ec401c6e
dyf102 <dyf102@gmail.com>
2021-02-11 21:15:52 -0500

    Add a greedy method

--- dp/python/longest-valid-parentheses.py
+++ dp/python/longest-valid-parentheses.py
@@ -15,3 +15,32 @@ class Solution:
                     dp[i] = dp[i - 1] + 2 + 0 if i - dp[i - 1] - 2 == -1 else dp[i - dp[i - 1] - 2]
         # print(dp)
         return max(dp)
+
+    def longestValidParentheses2(self, s: str) -> int:
+        """Greedy approach
+        """
+        n = len(s)
+        if n == 0:
+            return 0
+        left = right = 0
+        longest = 0
+        for i in range(n):
+            if s[i] == '(':
+                left += 1
+            else:
+                right += 1
+                if right > left:
+                    left = right = 0
+            if left == right:
+                longest = max(longest, left + right)
+        left = right = 0
+        for i in range(n)[::-1]:
+            if s[i] == ')':
+                left += 1
+            else:
+                right += 1
+                if right > left:
+                    left = right = 0 # reset
+            if left == right:
+                longest = max(longest,  left + right)
+        return longest
