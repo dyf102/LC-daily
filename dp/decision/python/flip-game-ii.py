7f77f02e3ad62c42bac25e48770ebcb5856ff0f9
dyf102 <dyf102@gmail.com>
2021-02-28 19:27:17 -0500

    Add mock, DP, Array questions

---
+++ dp/decision/python/flip-game-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canWin(self, s: str) -> bool:
+        cache = {}
+        n = len(s)
+        @lru_cache()
+        def dp(status):
+            if '++' not in status:
+                return False
+            for i in range(n - 1):
+                if status[i] == '+' and status[i + 1] == '+':
+                    if not dp(status[:i] + "--" + status[i + 2:]):
+                        return True
+            return False
+        return dp(s)
